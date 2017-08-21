5a9126dd3c59103b77544fe5849f71db85872801
Thomas Stuber <tstuber@gmail.com>
2017-08-21 14:59:31 +0000

    added pry breakpoint

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,11 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+
+### Uncomment to set a breakpoint. 
+#require 'pry'
+#binding.pry
+
 include_recipe 'httpd::install'
 include_recipe 'httpd::configuration'
 include_recipe 'httpd::service'
