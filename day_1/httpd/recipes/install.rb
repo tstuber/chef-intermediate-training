36821686950a54bdfb5c44dfb3381532de8a46dd
tstuber <tstuber@gmail.com>
2017-08-26 19:22:22 +0200

    apt_update for all OS

--- day_1/httpd/recipes/install.rb
+++ day_1/httpd/recipes/install.rb
@@ -3,15 +3,10 @@
 # Recipe:: install
 #
 # Copyright:: 2017, The Authors, All Rights Reserved
-#
+
 #require 'pry'
 #binding.pry
 
-# Platform-dependent attributes. 
-case node['platform']
-when 'ubuntu'
-  apt_update
-else
-end
+apt_update
 
 package node['httpd']['package_name']
